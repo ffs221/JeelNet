7b112b12b3c2d155523f3b6ed43cdfb6d8af1821
Yung Ju Rick Kim <yungjurick@gmail.com>
2019-04-27 15:32:20 +0400

    Add Toolbar

--- src/components/ToolBar.vue
+++ src/components/ToolBar.vue
@@ -1,11 +1,42 @@
 <template>
-  <section></section>
+  <div class="toolbar">
+    <section class="toolbar__left">
+      <img src alt="logo">
+    </section>
+    <section class="toolbar__right">
+      <router-link class="toolbar__right__link" to="/home">Home</router-link>
+      <router-link class="toolbar__right__link" to="/topics">Topics</router-link>
+      <router-link class="toolbar__right__link" to="/inbox">Inbox</router-link>
+      <router-link class="toolbar__right__link" to="/profile">Profile</router-link>
+    </section>
+  </div>
 </template>
 
-<script>
-export default {};
-</script>
-
 <style lang="scss">
+.toolbar {
+  position: fixed;
+  width: 100vw;
+  height: 7.5vh;
+  display: flex;
+  justify-content: space-between;
+  /* background-color: red; */
+  &__left {
+    display: flex;
+    align-items: center;
+    padding-left: 2vw;
+  }
+  &__right {
+    display: flex;
+    align-items: center;
+    padding: 0 2vw;
+    &__link {
+      color: black;
+      text-decoration: none;
+    }
+    &__link + &__link {
+      margin-left: 3vw;
+    }
+  }
+}
 </style>
 
